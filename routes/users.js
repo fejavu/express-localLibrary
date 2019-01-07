ecdcc8108ba50bc3d0c3b7ef337f98bef3e426d3
fejavu <fejavu@users.noreply.github.com>
2019-01-07 23:54:50 +0800

    MDN

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // Pratice of learning express app
-router.get('/cool/', function(req, res, next){
+router.get('/cool', function(req, res, next){
   res.send('Fejavu you are so cool!');
-});
-
-// Test page
-router.get('/Test',function(req, res, next) {
-  res.send('This is a router user test page.');
 });
 
 module.exports = router;
